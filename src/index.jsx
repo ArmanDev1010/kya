3ccbd204ae6d2c15bb6cdf190133cb32b64981a7
ArmanDev1010 <arman.manukyan24.y@gmail.com>
2024-01-07 10:08:45 +0400

    scroll to top

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop.js";
 
 i18next.init({
   interpolation: { escapeValue: false },
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <I18nextProvider i18n={i18next}>
         <App />
+        <ScrollToTop />
       </I18nextProvider>
     </BrowserRouter>
   </React.StrictMode>
